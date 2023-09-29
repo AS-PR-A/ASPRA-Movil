90b8101a8929156c0de9f5ee594377dd6c2e95e3
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2023-09-29 15:18:32 -0300

    Actualizacion de la pantalla de reporte
    Actualización de la Pantalla de Reporte, agregando un mensaje de alerta de "reporte enviado"

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/ReportarActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/ReportarActivity.java
@@ -1,10 +1,14 @@
 package com.example.aspra_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ReportarActivity extends AppCompatActivity {
 
@@ -12,11 +16,29 @@ public class ReportarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportar);
+        irReporteExitoso(ReportarActivity.this);
     }
 
-    public void irReporteExitoso(View view) {
-        Intent intent = new Intent(this, ReporteExitosoActivity.class);
-        startActivity(intent);
+    public void irReporteExitoso (Context context)
+    {
+        Button button = findViewById(R.id.button_reporte_enviar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Reporte");
+                builder.setMessage("Reporte enviado");
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(context, ReporteExitosoActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
 }
+
