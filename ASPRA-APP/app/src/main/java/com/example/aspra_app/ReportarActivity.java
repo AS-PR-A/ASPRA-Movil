cd57de2f339f26715e051be999d08b079004a0a4
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2023-09-29 23:51:20 -0300

    Actualizacion de la Activity Reportar
    Actualización del archivo "activity_reportar" agregando un desplegable en Motivo, ademas de caja de texto en Dirección y Descripción

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/ReportarActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/ReportarActivity.java
@@ -8,16 +8,28 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class ReportarActivity extends AppCompatActivity {
+
+    Spinner spMotivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportar);
         irReporteExitoso(ReportarActivity.this);
+
+        spMotivo = findViewById(R.id.spMotivo);
+
+        String[] Motivo = {"Maltrato", "Abandono", "Otro"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Motivo);
+
+        spMotivo.setAdapter(adaptador);
     }
+
 
     public void irReporteExitoso (Context context)
     {
@@ -27,7 +39,7 @@ public class ReportarActivity extends AppCompatActivity {
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setTitle("Reporte");
-                builder.setMessage("Reporte enviado");
+                builder.setMessage("Enviando reporte...");
                 builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -39,6 +51,5 @@ public class ReportarActivity extends AppCompatActivity {
             }
         });
     }
-
 }
 
