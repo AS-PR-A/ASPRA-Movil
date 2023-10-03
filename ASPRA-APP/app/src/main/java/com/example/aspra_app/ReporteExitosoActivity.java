418752dea7c770cec49397a6650ef90f0a992f10
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2023-10-02 21:37:48 -0300

    Add files via upload

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/ReporteExitosoActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/ReporteExitosoActivity.java
@@ -2,7 +2,9 @@ package com.example.aspra_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReporteExitosoActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class ReporteExitosoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_exitoso);
     }
+
+    public void redirigirAMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
