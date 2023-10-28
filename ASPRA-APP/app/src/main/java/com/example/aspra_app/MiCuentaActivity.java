545a6a3895fd32e54d0438c2f9161b2e38d18c05
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2023-10-28 16:40:49 -0300

    Alerta emergente confirmacion de Reporte eliminado
    Creación de las alertas emergentes de confirmación de Reporte eliminado y redireccion a Mis Reportes, ademas de la redireccion a la pantalla de Inicio al eliminar la cuenta

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/MiCuentaActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/MiCuentaActivity.java
@@ -1,10 +1,13 @@
 package com.example.aspra_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MiCuentaActivity extends AppCompatActivity {
 
@@ -18,5 +21,42 @@ public class MiCuentaActivity extends AppCompatActivity {
         Intent intent = new Intent(this, EditarCuentaActivity.class);
         startActivity(intent);
     }
+
+    public void eliminarCuenta(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Estás seguro de que quieres eliminar tu cuenta?")
+                .setCancelable(false)
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Acciones a realizar si el usuario confirma
+                        mostrarConfirmacion();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Acciones a realizar si el usuario cancela
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void mostrarConfirmacion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Cuenta eliminada")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Acciones a realizar al presionar OK
+                        Intent intent = new Intent(MiCuentaActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
 
 }
