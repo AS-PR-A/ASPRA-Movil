989f279526e82354e65d50934dd74c8736cdf5f9
JDGA1997 <105946879+JDGA1997@users.noreply.github.com>
2023-10-29 02:46:37 -0300

    Redireccion a la pagina ASPRA web
    Se agrego la redireccion a la pagina ASPRA web al realizar clic en la misma

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/ContactoActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/ContactoActivity.java
@@ -2,7 +2,11 @@ package com.example.aspra_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
 
 public class ContactoActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class ContactoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+    }
+
+    public void openWebsite(View view) {
+        Uri webpage = Uri.parse("https://ezmant.github.io/Proyecto_FullStack_ISPC/Maqueta/Vistas/index.html");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+// Si no hay una aplicación para manejar el intent, abre el navegador web.
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ezmant.github.io/Proyecto_FullStack_ISPC/Maqueta/Vistas/index.html"));
+            startActivity(browserIntent);
+        }
     }
 }
