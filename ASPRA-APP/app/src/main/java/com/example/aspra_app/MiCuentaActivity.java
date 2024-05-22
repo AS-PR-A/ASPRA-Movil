a9aab7e92ea78cb650d267f47335e1c12565a33e
Jazmin Amato <proyectos.amato@gmail.com>
2024-05-22 18:01:06 -0300

    Add functionality to delete user

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/MiCuentaActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/MiCuentaActivity.java
@@ -4,17 +4,52 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.aspra_app.data.UserDAO;
+import com.example.aspra_app.models.Usuario;
+
 
 public class MiCuentaActivity extends AppCompatActivity {
 
+    private Button buttonEliminar;
+    private UserDAO userDAO;
+    private Usuario usuario = new Usuario();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_cuenta);
+
+        buttonEliminar = findViewById(R.id.button_eliminar_cuenta);
+        userDAO = new UserDAO(this);
+
+
+        buttonEliminar.setOnClickListener(view -> {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setMessage("¿Estás seguro de que quieres eliminar tu cuenta?")
+                    .setCancelable(false)
+                    .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog,int which) {
+                            //userDAO.deleteUser(usuario.getEmail());
+                            userDAO.deleteUser("usuario3@gmail.com");
+                            Toast.makeText(MiCuentaActivity.this, "Cuenta eliminada!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MiCuentaActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create();
+            dialog.show();
+        });
     }
 
     public void irEditarCuenta(View view) {
@@ -22,35 +57,4 @@ public class MiCuentaActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void eliminarCuenta(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("¿Estás seguro de que quieres eliminar tu cuenta?")
-                .setCancelable(false)
-                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        mostrarConfirmacion();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
-
-    public void mostrarConfirmacion() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Cuenta eliminada")
-                .setCancelable(false)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(MiCuentaActivity.this, MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
 }
