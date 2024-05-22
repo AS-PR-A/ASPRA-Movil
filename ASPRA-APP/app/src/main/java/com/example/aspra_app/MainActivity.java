ef373dffdf679b02bd574f461947b49f73745201
Nico Mattos <106168031+NicolasMattos03@users.noreply.github.com>
2024-05-21 23:27:41 -0300

    Update Main
    Update Main

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/MainActivity.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,10 +36,18 @@ public class MainActivity extends AppCompatActivity {
        startActivity(intent);
     }
 
-    public void irMultimedia(View view) {
-        Intent intent = new Intent(this, MultimediaActivity.class);
-        startActivity(intent);
+
+    public void openWebsite(View view) {
+        Uri webpage = Uri.parse("https://link.mercadopago.com.ar/aspra");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://link.mercadopago.com.ar/aspra"));
+            startActivity(browserIntent);
+        }
     }
+
 
     public void irMisReportes(View view) {
         Intent intent = new Intent(this, MisReportesActivity.class);
