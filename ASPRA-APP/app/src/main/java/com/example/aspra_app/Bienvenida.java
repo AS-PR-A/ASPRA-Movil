4b69f69209afe8f43b42e4b7370c2bc9fa8b33bb
Nico Mattos <106168031+NicolasMattos03@users.noreply.github.com>
2024-06-01 22:55:25 -0300

    Update activity_bienvenida.xml

--- ASPRA-APP/app/src/main/java/com/example/aspra_app/Bienvenida.java
+++ ASPRA-APP/app/src/main/java/com/example/aspra_app/Bienvenida.java
@@ -1,8 +1,11 @@
 package com.example.aspra_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 //import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +20,30 @@ public class Bienvenida extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bienvenida);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+                VideoView video = (VideoView) findViewById(R.id.videoView);
+        Uri path = Uri.parse("android.resource://com.example.aspra_app/" + R.raw.inicio);
+        video.setVideoURI(path);
+        video.setMediaController(new MediaController(this));
+        video.start();
+        video.requestFocus();
+
+
+        ;
     }
+
+
+
+
+
+
+
+
 
     public void irLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
